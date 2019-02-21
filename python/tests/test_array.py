68a431607aae0e5cdae718f65f126309521ed47c
David Wendt <dwendt@nvidia.com>
2019-02-21 09:13:43 -0500

    add support for ndarray, buffer in getitem/gather

--- python/tests/test_array.py
+++ python/tests/test_array.py
@@ -13,12 +13,15 @@ strs = nvstrings.to_device(["abc","defghi",None,"jkl","mno","pqr","stu","dog and
 print(strs)
 
 print(".gather([1,3,5,7]):  ",strs.gather([1,3,5,7]))
-d_arr = rmm.to_device(np.array([1,3,5,7],dtype=np.int32))
+arr = np.array([1,3,5,7],dtype=np.int32)
+d_arr = rmm.to_device(arr)
 devmem = d_arr.device_ctypes_pointer.value
 print(".gather([1,3,5,7],4):",strs.gather(devmem,4))
 print("[3]:",strs[3])
 print("[[1,3,5,7]]:",strs[[1,3,5,7]])
 print("[1:7:2]:",strs[1:7:2])
+print("[arr]:",strs[arr])
+print("[d_arr]:",strs[d_arr])
 
 print(".remove_strings([2,4,6,8]):  ",strs.remove_strings([2,4,6,8]))
 d_arr = rmm.to_device(np.array([2,4,6,8],dtype=np.int32))
