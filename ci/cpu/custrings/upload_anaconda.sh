2df9f48c547f7adfbcbeadee913c922790b34fa6
Mike Wendt <mwendt@nvidia.com>
2019-03-12 17:07:23 -0400

    ENH Add conda upload script

---
+++ ci/cpu/custrings/upload_anaconda.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -xe
+
+if [ "$UPLOAD_PACKAGE" == '1' ]; then
+    export UPLOADFILE=`conda build -c defaults -c conda-forge --python=${PYTHON} conda/recipes/nvstrings --output`
+    SOURCE_BRANCH=master
+
+    test -e ${UPLOADFILE}
+    CUDA_REL=${CUDA:0:3}
+    if [ "${CUDA:0:2}" == '10' ]; then
+    # CUDA 10 release
+    CUDA_REL=${CUDA:0:4}
+    fi
+
+    LABEL_OPTION="--label dev --label cuda${CUDA_REL}"
+    if [ "${LABEL_MAIN}" == '1' ]; then
+    LABEL_OPTION="--label main --label cuda${CUDA_REL}"
+    fi
+    echo "LABEL_OPTION=${LABEL_OPTION}"
+
+    if [ -z "$MY_UPLOAD_KEY" ]; then
+    echo "No upload key"
+    return 0
+    fi
+
+    echo "Upload"
+    echo ${UPLOADFILE}
+    anaconda -t ${MY_UPLOAD_KEY} upload -u rapidsai ${LABEL_OPTION} --force ${UPLOADFILE}
+else
+    echo "Skipping upload"
+fi
