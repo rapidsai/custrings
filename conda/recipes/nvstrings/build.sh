45ea2f0ba2104a25c109bdf8a44b3a7d49b34c3d
Keith Kraus <keith.j.kraus@gmail.com>
2019-03-13 15:31:00 -0400

    Update conda/recipes/nvstrings/build.sh
    Co-Authored-By: mike-wendt <mike.wendt@me.com>

--- conda/recipes/nvstrings/build.sh
+++ conda/recipes/nvstrings/build.sh
@@ -2,6 +2,6 @@
 cd python
 $PYTHON setup.py install
 cp ../cpp/include/NVStrings.h $PREFIX/include/nvstrings
-cp ../cpp/include/NVCategory.h $PREFIX/include
+cp ../cpp/include/NVCategory.h $PREFIX/include/nvstrings
 cp build/lib.linux-x86_64*/libNVStrings.so $PREFIX/lib
 cp build/lib.linux-x86_64*/libNVCategory.so $PREFIX/lib
