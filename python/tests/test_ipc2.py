d0353e36ec42f4041658fdcd63de5f0323f2cfbf
Rommel <rommel@blazingdb.com>
2019-04-04 15:11:03 -0500

    refactored IPC Python test

--- python/tests/test_ipc2.py
+++
@@ -1,9 +0,0 @@
-import nvstrings
-import pickle
-
-filehandler = open("/tmp/ipctest", 'rb') 
-ipc_data = pickle.load(filehandler)
-#print(ipc_data)
-
-new_strs = nvstrings.create_from_ipc(ipc_data)
-print(new_strs)
