a14523378d58ce569fb7b114324494519bc6bb3d
Keith Kraus <kkraus@nvidia.com>
2019-04-09 11:34:58 -0400

    Pretty sure this works but gives a warning about empty package name

--- python/setup.py
+++ python/setup.py
@@ -10,20 +10,30 @@ cuda_version = ''.join(os.environ.get('CUDA_VERSION', 'unknown')
                          .split('.')[:2])
 name = 'nvstrings-cuda{}'.format(cuda_version)
 version = os.environ.get('GIT_DESCRIBE_TAG', '0.0.0.dev0').lstrip('v')
-setup(name=name,
-      description='CUDA strings Python bindings',
-      version=version,
-      py_modules=['nvstrings', 'nvcategory', 'nvtext'],
-      url='https://github.com/rapidsai/custrings',
-      author='NVIDIA Corporation',
-      license="Apache",
-      install_requires=install_requires,
-      packages=find_packages(),
-      package_data={
-          '': ['pyniNVStrings.so', 'pyniNVCategory.so', 'pyniNVText.so'],
-      },
-      include_package_data=True,
-      zip_safe=False
-      )
+setup(
+    name=name,
+    description='CUDA strings Python bindings',
+    url='https://github.com/rapidsai/custrings',
+    version=version,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+    py_modules=['nvstrings', 'nvcategory', 'nvtext'],
+    author='NVIDIA Corporation',
+    license="Apache",
+    install_requires=install_requires,
+    data_files=[('', ['pyniNVStrings.so', 'pyniNVCategory.so', 'pyniNVText.so'])],
+    packages=[''],
+    package_data={
+        '': [
+            'pyniNVStrings.so', 'pyniNVCategory.so', 'pyniNVText.so'
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False
+)
 
 convert_to_manylinux(name, version)
