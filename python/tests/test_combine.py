58ef61e491a8041c551d7d63201a4b0760fb1eaa
Nick Becker <nickb500@gmail.com>
2019-04-29 14:14:58 -0400

    join and cat tests

--- python/tests/test_combine.py
+++ python/tests/test_combine.py
@@ -1,28 +1,47 @@
-#
+# Copyright (c) 2018-2019, NVIDIA CORPORATION.
+
+import pytest
+
 import nvstrings
 
-#
-from librmm_cffi import librmm as rmm
-from librmm_cffi import librmm_config as rmm_cfg
-rmm_cfg.use_pool_allocator = True 
-rmm.initialize()
+from utils import assert_eq
 
-#
-strs1 = nvstrings.to_device(["abc","def",None,"","jkl","mno","accént"])
-print("strs1:",strs1)
-print(".cat():",strs1.cat())
-print(".cat(sep=:):",strs1.cat(sep=":"))
-print(".cat(sep=:,na_rep=_):",strs1.cat(sep=":",na_rep="_"))
-print(".cat([1,2,3,4,5,é,nil],sep=:,na_rep=_):",strs1.cat(["1","2","3","4","5","é",None],sep=":",na_rep="_"))
 
-strs2 = nvstrings.to_device(["1","2","3",None,"5","é",""])
-print("strs2:",strs2)
-print("strs1.cat(strs2):",strs1.cat(strs2))
-print("strs1.cat(strs2,sep=:):",strs1.cat(strs2,sep=":"))
-print("strs1.cat(strs2,sep=:,na_rep=_):",strs1.cat(strs2,sep=":",na_rep="_"))
+def test_cat():
+    strs = nvstrings.to_device(["abc", "def", None, "", "jkl", "mno", "accént"])
+    got = strs.cat()
+    expected = ['abcdefjklmnoaccént']
+    assert_eq(got, expected)
 
-print("strs1.join():",strs1.join())
-print("strs1.join(sep=:):",strs1.join(sep=":"))
+    # non-default separator
+    got = strs.cat(sep=':')
+    expected = ['abc:def::jkl:mno:accént']
+    assert_eq(got, expected)
 
-strs1 = None
-strs2 = None
+    # non default separator and na_rep
+    got = strs.cat(sep=':', na_rep='_')
+    expected = ['abc:def:_::jkl:mno:accént']
+    assert_eq(got, expected)
+
+    # non-null others, default separator, and na_rep
+    got = strs.cat(["1", "2", "3", "4", "5", "é", None], sep=":", na_rep="_")
+    expected = ['abc:1', 'def:2', '_:3', ':4', 'jkl:5', 'mno:é', 'accént:_']
+    assert_eq(got, expected)
+
+    # nvstrings others
+    strs2 = = nvstrings.to_device(["1", "2", "3", None, "5", "é", ""])
+    got = strs.cat(strs2)
+    expected = ['abc1', 'def2', None, None, 'jkl5', 'mnoé', 'accént']
+    assert_eq(got, expected)
+
+
+def test_join():
+    strs = nvstrings.to_device(["1", "2", "3", None, "5", "é", ""])
+    got = strs.join()
+    expected = ['1235é']
+    assert_eq(got, expected)
+
+    # non-default sep
+    got = strs.join(sep=':')
+    expected = ['1:2:3:5:é:']
+    assert_eq(got, expected)
