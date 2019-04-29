c6b0a2f5c8791aad5948df0d6863bbcf9b1831fe
Nick Becker <nickb500@gmail.com>
2019-04-29 14:49:52 -0400

    strings offsets tests

--- python/tests/test_offsets.py
+++ python/tests/test_offsets.py
@@ -1,58 +1,64 @@
+# Copyright (c) 2018-2019, NVIDIA CORPORATION.
+
+import pytest
+import numpy as np
 
 import nvstrings
-import numpy as np
-values = np.array([97, 112, 112, 108, 101], dtype=np.int8)
-print("values",values.tobytes())
-offsets = np.array([0,1,2,3,4,5], dtype=np.int32)
-print("offsets",offsets)
-s = nvstrings.from_offsets(values,offsets,5)
-print(s)
 
-bitmask = np.array([29], dtype=np.int8)
-print("bitmask",bitmask.tobytes())
-s = nvstrings.from_offsets(values,offsets,5,bitmask,1)
-print(s)
+from librmm_cffi import librmm as rmm
 
-print("------------------")
-values = np.array([97, 112, 112, 108, 101, 112, 101, 97, 114], dtype=np.int8)
-print("values",values.tobytes())
-offsets = np.array([0,5,5,9], dtype=np.int32)
-print("offsets",offsets)
-s = nvstrings.from_offsets(values,offsets,3)
-print(s)
+from utils import assert_eq
 
-bitmask = np.array([5], dtype=np.int8)
-print("bitmask",bitmask.tobytes())
-s = nvstrings.from_offsets(values,offsets,3,bitmask,1)
-print(s)
 
-print("values.ctypes.data",hex(values.ctypes.data))
-print("offsets.ctypes.data",hex(offsets.ctypes.data))
-print("bitmask.ctypes.data",hex(bitmask.ctypes.data))
-s = nvstrings.from_offsets(values.ctypes.data,offsets.ctypes.data,3,bitmask.ctypes.data,1)
-print(s)
+def test_from_offsets():
+    values = np.array([97, 112, 112, 108, 101], dtype=np.int8)
+    offsets = np.array([0, 1, 2, 3, 4, 5], dtype=np.int32)
+    s = nvstrings.from_offsets(values, offsets, 5)
+    expected = ['a', 'p', 'p', 'l', 'e']
+    assert_eq(s, expected)
 
-print("------------------")
-s = nvstrings.to_device(['a','p','p','l','e'])
-values = np.empty(s.size(), dtype=np.int8)
-offsets = np.empty(s.size()+1, dtype=np.int32)
-nulls = np.empty(int(s.size()/8)+1, dtype=np.int8)
-s.to_offsets(values,offsets,nulls)
-print("values",values.tobytes())
-print("offsets",offsets)
-print("nulls",nulls.tobytes())
+    values = np.array([97, 112, 112, 108, 101, 112, 101, 97, 114],
+                      dtype=np.int8)
+    offsets = np.array([0, 5, 5, 9], dtype=np.int32)
+    s = nvstrings.from_offsets(values, offsets, 3)
+    expected = ['apple', '', 'pear']
+    assert_eq(s, expected)
 
-print("------------------")
-import nvcategory
 
-values = np.array([97, 112, 112, 108, 101], dtype=np.int8)
-print("values",values.tobytes())
-offsets = np.array([0,1,2,3,4,5], dtype=np.int32)
-print("offsets",offsets)
-c = nvcategory.from_offsets(values,offsets,5)
-print(c.keys(),c.values())
+def test_from_offsets_with_bitmask():
+    values = np.array([97, 112, 112, 108, 101], dtype=np.int8)
+    offsets = np.array([0, 1, 2, 3, 4, 5], dtype=np.int32)
+    bitmask = np.array([29], dtype=np.int8)
+    s = nvstrings.from_offsets(values, offsets, 5, bitmask, 1)
+    expected = ['a', None, 'p', 'l', 'e']
+    assert_eq(s, expected)
 
-bitmask = np.array([29], dtype=np.int8)
-print("bitmask",bitmask.tobytes())
-c = nvcategory.from_offsets(values,offsets,5,bitmask,1)
-print(c.keys(),c.values())
+
+def test_from_offsets_ctypes_data():
+    values = np.array([97, 112, 112, 108, 101, 112, 101, 97, 114],
+                      dtype=np.int8)
+    offsets = np.array([0, 5, 5, 9], dtype=np.int32)
+    bitmask = np.array([5], dtype=np.int8)
+    s = nvstrings.from_offsets(values.ctypes.data, offsets.ctypes.data, 3,
+                               bitmask.ctypes.data, 1)
+    expected = ['apple', None, 'pear']
+    assert_eq(s, expected)
+
+
+def test_to_offsets():
+    s = nvstrings.to_device(['a', 'p', 'p', 'l', 'e'])
+    values = np.empty(s.size(), dtype=np.int8)
+    offsets = np.empty(s.size() + 1, dtype=np.int32)
+    nulls = np.empty(int(s.size() / 8) + 1, dtype=np.int8)
+    s.to_offsets(values, offsets, nulls)
+
+    expected_values = [97, 112, 112, 108, 101]
+    expected_offsets = [0, 1, 2, 3, 4, 5]
+    expected_nulls = [31]
+
+    assert np.array_equal(values, expected_values)
+    assert np.array_equal(offsets, expected_offsets)
+    assert np.array_equal(nulls, expected_nulls)
+
+
+
