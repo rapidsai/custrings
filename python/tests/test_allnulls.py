5672d4344da97d3f0eeb406bdafe1e7064a3c24b
Nick Becker <nickb500@gmail.com>
2019-04-30 11:27:28 -0400

    test allnulls

--- python/tests/test_allnulls.py
+++ python/tests/test_allnulls.py
@@ -1,5 +1,19 @@
 
 import nvstrings
+import pytest
+from utils import methodcaller
+
+@pytest.mark.parametrize('func',
+                         ['lower', 'upper',
+                          'swapcase', 'capitalize',
+                          'title', 'strip'])
+def test_allnulls(func):
+
+   strs = nvstrings.to_device([None, None, None])
+   M = methodcaller(func)
+
+   assert M(strs).to_host() == [None, None, None]
+
 
 #
 strs = nvstrings.to_device([None,None,None,None,None,None,None,None])
