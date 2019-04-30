960a600f9c06be78f4920248a2a8e8105c84b77f
Nick Becker <nickb500@gmail.com>
2019-04-30 12:42:49 -0400

    sublist test

--- python/tests/test_array.py
+++ python/tests/test_array.py
@@ -10,6 +10,12 @@ def test_gather():
     assert got.to_host() == expected
 
 
+def test_sublist():
+    strs = nvstrings.to_device(["abc", "defghi", None,"cat"])
+    got = strs.sublist([1,3,2])
+    expected = ['defghi', 'cat', None]
+    assert got.to_host() == expected
+
 def test_remove_strings():
     strs = nvstrings.to_device(["abc", "defghi", None,"cat"])
     got = strs.remove_strings([0,2])
