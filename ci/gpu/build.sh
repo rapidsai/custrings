5ff2088ccde37335e4a217cd8f5b73a5d8738154
Nick Becker <nickb500@gmail.com>
2019-05-02 13:29:45 -0400

    update GPU CI to use librmm_cffi

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -37,7 +37,7 @@ env
 
 logger "Activate conda env..."
 source activate gdf
-conda install -y 'librmm==0.7.*' 'cmake_setuptools>=0.1.3'
+conda install -y 'librmm_cffi==0.7.*' 'cmake_setuptools>=0.1.3'
 
 logger "Check versions..."
 python --version
